7555a445bd6f42a5637858036a2e7cad80b3c280
Ayush Bhat <67255324+AyushBhat822@users.noreply.github.com>
2022-08-19 01:21:36 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import pendulum
+from datetime import datetime
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+metrics.info('app_info', 'Application info', version='1.0.3')
+
+@app.route("/")
+@metrics.do_not_track()
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/time")
+def current_time():
+    ist = pendulum.timezone('Asia/Calcutta')
+    now = datetime.now(ist)
+    current_time = now.strftime("%H:%M:%S")
+    return (current_time)
